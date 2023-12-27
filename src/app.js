5e0cd0b04eba9a2e6ab22822d8f001c8aec0adfc
Alex_Bond <bondarenko.aleksandr.25@gmail.com>
2023-12-27 08:15:58 +0600

    added dependency on-change

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-'use strict';
+import { MainView } from "./views/main/main";
+
+
+class App {
+    routes = [
+        { path: '', view: MainView }
+    ]
+
+    appState = {
+        favorites : []
+    }
+
+    constructor(){
+        window.addEventListener('hashchange', this.route.bind(this));
+        this.route();
+    }
+
+    route(){
+        if(this.currentView){
+            this.currentView.destroy();
+        }
+        const view = this.routes.find(route => route.path == location.hash).view
+        this.currentView = new view(this.appState);
+        this.currentView.render();
+    }
+}
+
+new App();
---
+++ src/views/main/main.js
@@ -0,0 +1,29 @@
+import { AbstractView } from "../../common/view.js";
+import onChange from 'on-change';
+
+export  class MainView extends AbstractView {
+    state = {
+        list: [],
+        loading: false,
+        searchQuery: undefined,
+        offset: 0
+    }
+    constructor(appState){
+        super();
+        this.appState = appState;
+        this.appState = onChange(this.appState, this.appStateHook.bind(this));
+        this.setTitle('Search books');
+    }
+
+    appStateHook(path){
+        if (path === 'favorites'){
+            console.log(path);
+        }
+    }
+    render() {
+        const main = document.createElement('div');
+        main.innerHTML = 'test';
+        this.app.innerHTML = '';
+        this.app.append(main);
+    }
+}
